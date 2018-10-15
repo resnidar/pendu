34aa7e895428934e7fa3a4a36d52c30b80f0aa01
resnidar <remy.snidaro@gmail.com>
2018-10-15 18:17:53 +0200

    les tableaux sont fonctionnel ,tableau de char OK et fonctionnel ,le travail continue

--- src/com/resnidar/LogicOfGame.java
+++ src/com/resnidar/LogicOfGame.java
@@ -1,29 +1,29 @@
 package com.resnidar;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class LogicOfGame {
-    public void logic(){
+    public void logic() {
+        Scanner sc = new Scanner(System.in);
         display(1);
         display(2);
-        wordPrepar();
+        char[] wordTab = wordPrepar();
+        Game(wordTab);
     }
 
     public void display(int number) {
 
         if (number == 1) {
             System.out.println("Bienvenue dans le jeu de pendu ,je suppose que vous connaissez les régles alors commencons !");
-        }
-        else if (number == 2) {
+        } else if (number == 2) {
             System.out.println("voulez vous lancé la partie ?");
-        }
-        else
-        {
+        } else {
             System.out.println("erreur");
         }
     }
 
-    public void wordPrepar(){
+    public char[] wordPrepar() {
         String word = null;
         int wordlength = 0;
         try {
@@ -31,21 +31,18 @@ public class LogicOfGame {
         } catch (IOException e) {
             System.out.println("il y a un probleme dans la lecture du csv");
         }
-        for (int i = 0; i < word.length(); i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             wordlength = i + 1;
         }
         char wordTab[] = new char[wordlength];
-        for (int i = 0; i < word.length() ;i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             System.out.println(i);
             wordTab[i] = word.charAt(i);
         }
-        System.out.println(wordTab[2]);
-        wordTab[2] = 'b';
-        for (int i = 0; i < word.length(); i++)
-        {
-            System.out.print(wordTab[i]);
-        }
+        return wordTab;
+    }
+
+    public void Game(char[] wordTab) {
+        /* en cour de travail */
     }
 }
