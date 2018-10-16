c76f2991ee28f85fdf7a62623a891190030af88e
resnidar <remy.snidaro@gmail.com>
2018-10-16 17:02:57 +0200

    mise en place d'un génerateur aleatoire de int pour selectionné les mots dans le csv

--- src/com/resnidar/InspectCSV.java
+++ src/com/resnidar/InspectCSV.java
@@ -5,13 +5,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Random;
 
 public class InspectCSV {
 
     public static String read() throws IOException {
+        Random random = new Random();
+        int randomNumber = random.nextInt(50);
         Path wordListPath = Paths.get("wordList.csv");
         List<String> lines = Files.readAllLines(wordListPath);
-        String word = lines.get(0).split(" ")[0];
+        String word = lines.get(0).split(",")[randomNumber];
+        System.out.println("le nombre aleatoire choisi est : " + randomNumber);
         return word;
     }
 }
